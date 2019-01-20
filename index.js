61498fbf4dd1ee775dbaed5e5c6445a46b6db445
Trooper-Toaster <46659742+Trooper-Toaster@users.noreply.github.com>
2019-01-20 16:24:36 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const botconfig = require("./botconfig.json");
 const Discord = require("discord.js");
-const fs = require("fs");
 const bot = new Discord.Client({disableEveryone: true});
 bot.on("ready", async () => {
   console.log(`${bot.user.username} is online`);
@@ -9,19 +8,7 @@ bot.on("ready", async () => {
 bot.commands = new Discord.Collection();
 
 
-fs.readdir("./commands/", (err, files) => {
-  if(err) console.log(err);
-  let jsfile = files.filter(f => f.split(".").pop() === "js")
-  if(jsfile.length <= 0){
-    console.log("Cant find anything");
-    return;
-  }
-  jsfile.forEach((f, i) =>{
-    let props = require(`./commands/${f}`);
-    console.log(`${f} loaded!`);
-    bot.commands.set(props.help.name, props);
-  });
-});
+
 
 
 
@@ -33,8 +20,7 @@ bot.on("message", async message=> {
   let messageArray = message.content.split(" ");
   let cmd = messageArray[0];
   let args = messageArray.slice(1);
-  let commandfile = bot.commands.get(cmd.slice(prefix.length));
-  if(commandfile) commandfile.run(bot,message,args);
+  
   
 if(cmd ===`${prefix}serverinfo`){
   let sicon = message.guild.displayAvatarURL;
