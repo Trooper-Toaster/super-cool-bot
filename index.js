4f4792e8913bc264d6006e733e227af3ca78845c
Trooper-Toaster <46659742+Trooper-Toaster@users.noreply.github.com>
2019-01-20 16:23:01 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,29 @@
 const botconfig = require("./botconfig.json");
 const Discord = require("discord.js");
+const fs = require("fs");
 const bot = new Discord.Client({disableEveryone: true});
 bot.on("ready", async () => {
   console.log(`${bot.user.username} is online`);
   bot.user.setGame("with the Breeze Dev Team");
 });
+bot.commands = new Discord.Collection();
+
+
+fs.readdir("./commands/", (err, files) => {
+  if(err) console.log(err);
+  let jsfile = files.filter(f => f.split(".").pop() === "js")
+  if(jsfile.length <= 0){
+    console.log("Cant find anything");
+    return;
+  }
+  jsfile.forEach((f, i) =>{
+    let props = require(`./commands/${f}`);
+    console.log(`${f} loaded!`);
+    bot.commands.set(props.help.name, props);
+  });
+});
+
+
 
 bot.on("message", async message=> {
   if(message.author.bot) return;
@@ -14,7 +33,8 @@ bot.on("message", async message=> {
   let messageArray = message.content.split(" ");
   let cmd = messageArray[0];
   let args = messageArray.slice(1);
-  
+  let commandfile = bot.commands.get(cmd.slice(prefix.length));
+  if(commandfile) commandfile.run(bot,message,args);
   
 if(cmd ===`${prefix}serverinfo`){
   let sicon = message.guild.displayAvatarURL;
@@ -28,17 +48,15 @@ if(cmd ===`${prefix}serverinfo`){
 
   return message.channel.send(serverinbed);
 }
-  
-  
-  if(cmd ===`${prefix}cmds`){
-  let serverinbed = new Discord.RichEmbed()
-  message.channel.send("```Warn, Kick, Clear, Ban```")
-}
+
+
+
+
 if(cmd === `${prefix}kick`){
 let kUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
 if(!kUser) return message.channel.send("Cant Find user");
 let kreason = args.join(" ").slice(22);
-if(!message.member.hasPermission("KICK_MEMBERS")) return message.send("Nice Try")
+if(!message.member.hasPermission("KICK_MEMBERS")) return message.channel.send("Nope")
 if(kUser.hasPermission("KICK_MEMBERS")) return message.channel.send("Cant Kick other Mods")
 
 let kickEmbed = new Discord.RichEmbed()
@@ -54,18 +72,36 @@ let kickChannel = message.guild.channels.find(`name`, "modlog")
 if(!kickChannel) return message.channel.send("Cant Find the ModLog")
 
 message.guild.member(kUser).kick(kreason);
-kUser.sendMessage(`You were kicked for ${kreason}`);
 kickChannel.send(kickEmbed);
+kUser.sendMessage(`You have been kicked for ${kreason}`);
 
 
+  return;
+}
+if(cmd === "toaster"){
+message.channel.send("is the best scripter")
 
+  return;
+}
+if(cmd === "trooper"){
+message.channel.send("is the best player")
+
+  return;
+}
+if(cmd === "bangles"){
+message.channel.send("is the best begger")
+
+  return;
+}
+if(cmd === `${prefix}cmds`){
+message.channel.send("Kick, Ban, Warn, Language, Clear, AddRole, RemoveRole, Role Lock, serverinfo, and botinfo")
   return;
 }
 if(cmd === `${prefix}warn`){
 let wUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
 if(!wUser) return message.channel.send("Cant Find user");
 let wreason = args.join(" ").slice(22);
-if(!message.member.hasPermission("KICK_MEMBERS")) return message.send("Nice Try")
+if(!message.member.hasPermission("KICK_MEMBERS")) return message.reply("Nice Try")
 
 
 let warnEmbed = new Discord.RichEmbed()
@@ -81,10 +117,149 @@ let warnChannel = message.guild.channels.find(`name`, "modlog")
 if(!warnChannel) return message.channel.send("Cant Find the ModLog")
 
 message.channel.send("User has been Warned!");
-wUser.sendMessage(`You have been warned for ${wreason}`);
 warnChannel.send(warnEmbed);
+wUser.sendMessage(`You have been warned for ${wreason}`);
 return;
 
+}
+if(cmd === `${prefix}mute`){
+let eqUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
+if(!eqUser) return message.channel.send("Cant Find user");
+if(!message.member.hasPermission("KICK_MEMBERS")) return message.send("Nice Try");
+
+
+return;
+}
+if(cmd === `${prefix}language`){
+  let lUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
+  if(!lUser) return message.channel.send("Cant Find user");
+  if(!message.member.hasPermission("KICK_MEMBERS")) return message.send("Nice Try");
+let fwarnEmbed = new Discord.RichEmbed()
+.setDescription("Language Violation")
+.setColor("#e8f442")
+.addField("Busted by", `${lUser} with ID ${lUser.id}`)
+.addField("Busted For", `<@${message.author.id}> with ID ${message.author.id}`)
+.addField("Time", message.createdAt)
+.addField("Busted in", message.channel)
+
+
+let warnfChannel = message.guild.channels.find(`name`, "modlog")
+if(!warnfChannel) return message.channel.send("Cant Find the ModLog")
+
+message.channel.send("User has been noted for a language violation!");
+warnfChannel.send(fwarnEmbed);
+lUser.sendMessage(`You have been warned for a language violation in ${message.channel}, please remember that children play this game and should not be exposed to it`);
+return;
+}
+
+if(cmd ===`${prefix}lock`){
+  if(!message.member.hasPermission("KICK_MEMBERS")) return message.channel.send(`Hello`);
+  let role = args[0];
+  let lereason = args.join(" ").slice(22);
+  if(!role) return message.reply("What Role?");
+  let jRole = message.guild.roles.find(`name`, role);
+  message.channel.overwritePermissions(jRole, {
+  SEND_MESSAGES: false
+});
+let ewarnEmbed = new Discord.RichEmbed()
+.setDescription("Role Lock")
+.setColor("#e8f442")
+.addField("Locked By",  `<@${message.author.id}> with ID ${message.author.id}`)
+.addField("Locked", args[0])
+.addField("Time", message.createdAt)
+
+
+
+let ewarnfChannel = message.guild.channels.find(`name`, "modlog")
+if(!ewarnfChannel) return message.channel.send("Cant Find the ModLog")
+ewarnfChannel.send(ewarnEmbed);
+message.channel.send(`**The role ${jRole.name} has been locked by an admin**`);
+
+  return;
+}
+if(cmd ===`${prefix}unlock`){
+  if(!message.member.hasPermission("KICK_MEMBERS")) return message.channel.send(`Hello`);
+  let role = args[0]
+  if(!role) return message.reply("What Role?");
+  let ejRole = message.guild.roles.find(`name`, role);
+  message.channel.overwritePermissions(ejRole, {
+  SEND_MESSAGES: true
+});
+let qewarnEmbed = new Discord.RichEmbed()
+.setDescription("Role Unlock")
+.setColor("#e8f442")
+.addField("Unlocked By",  `<@${message.author.id}> with ID ${message.author.id}`)
+.addField("Unlocked", args[0])
+.addField("Time", message.createdAt)
+
+
+
+let eewarnfChannel = message.guild.channels.find(`name`, "modlog")
+if(!eewarnfChannel) return message.channel.send("Cant Find the ModLog")
+eewarnfChannel.send(qewarnEmbed);
+message.channel.send(`**The role ${ejRole.name} has been unlocked by an admin**`);
+
+
+
+  return;
+}
+
+if(cmd === `${prefix}addrole`){
+if(!message.member.hasPermission("KICK_MEMBERS")) return message.channel.send("Nope");
+let rMember = message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[0]);
+if(!rMember) return message.reply("Role a Member");
+let role = args.join(" ").slice(22);
+if(!role) return message.reply("What Role?");
+let gRole = message.guild.roles.find(`name`, role);
+if(!gRole) return message.reply("How bout a role that is real?");
+
+if(rMember.roles.has(gRole.id)) return message.reply("They already have that role!");
+await(rMember.addRole(gRole.id));
+
+
+let froleimbed = new Discord.RichEmbed()
+  .setDescription("Added Role")
+  .setColor("#f4a142")
+  .addField("Time", message.createdAt)
+  .addField("Roled", rMember)
+  .addField("Role", gRole)
+  .addField("By", message.author);
+
+  let frole = message.guild.channels.find(`name`, "modlog");
+  if(!frole) return message.channel.send("Cant Find the ModLog");
+ frole.send(froleimbed);
+ message.channel.send(`Roled ${rMember} the ${gRole.name} Role`);
+
+  return;
+}
+if(cmd === `${prefix}removerole`){
+if(!message.member.hasPermission("KICK_MEMBERS")) return message.channel.send("Nope");
+let qMember = message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[0]);
+if(!qMember) return message.reply("Role a Member");
+let role = args.join(" ").slice(22);
+if(!role) return message.reply("What Role?");
+let tRole = message.guild.roles.find(`name`, role);
+if(!tRole) return message.reply("How bout a role that is real?");
+
+if(qMember.roles.has(tRole.id));
+await(qMember.removeRole(tRole.id));
+
+
+let roleimbed = new Discord.RichEmbed()
+  .setDescription("Removed Role")
+  .setColor("#f4a142")
+  .addField("Time", message.createdAt)
+  .addField("Who", qMember)
+  .addField("Removed", tRole)
+  .addField("By", message.author);
+
+  let frole = message.guild.channels.find(`name`, "modlog");
+  if(!frole) return message.channel.send("Cant Find the ModLog");
+ frole.send(roleimbed);
+ message.channel.send(`Removed ${qMember} ${tRole.name} Role`);
+
+
+  return;
 }
 if(cmd === `${prefix}ban`){
 let bUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
@@ -96,26 +271,30 @@ if(bUser.hasPermission("BAN_MEMBERS")) return message.channel.send("Can't ban ot
 let banembed = new Discord.RichEmbed()
 .setDescription("Ban")
 .setColor("#e09d0e")
-.addField("Banned User", `${kUser} with ID ${kUser.id}`)
+.addField("Banned User", `${bUser} with ID ${bUser.id}`)
 .addField("Banned By", `<@${message.author.id}> with ID ${message.author.id}`)
 .addField("Time", message.createdAt)
 .addField("Banned in", message.channel)
-.addField("Reason", kreason);
+.addField("Reason", breason);
 
 let kickChannel = message.guild.channels.find(`name`, "modlog")
 if(!kickChannel) return message.channel.send("Cant Find the ModLog")
 
-message.guild.member(bUser).ban(breason);
- bUser.sendMessage(`You were kicked for ${breason}`);
+message.guild.member(bUser).ban(kreason);
 kickChannel.send(banembed);
+bUser.sendMessage(`You have been warned for ${breason}`);
 
 
 
   return;
 }
+
+
+
 if(cmd ===`${prefix}clear`){
 if(!message.member.hasPermission("KICK_MEMBERS")) return message.reply("Nope");
-if(!args[0]) return message.channel.send("Whoops");
+if(!args[0]) return message.channel.send("Specify a Number").then(msg => msg.delete(5000));
+if(!args[1]) return message.channel.send("Specify a Reason!").then(msg => msg.delete(5000));
 message.channel.bulkDelete(args[0]).then(() => {
 
   let clearembed = new Discord.RichEmbed()
@@ -123,6 +302,7 @@ message.channel.bulkDelete(args[0]).then(() => {
   .setColor("#e09d0e")
   .addField("Cleared by", message.author)
   .addField("Cleared", args[0])
+  .addField("Reason", args[1])
   .addField("Time", message.createdAt);
 
 
@@ -149,3 +329,4 @@ message.channel.bulkDelete(args[0]).then(() => {
 
 
 bot.login(botconfig.token);
+
